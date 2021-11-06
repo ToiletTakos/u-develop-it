c1f1a66bd4f56f02d7b4045609bede1d72078d92
Dan <dan.pandolfo@gmail.com>
2021-11-05 20:58:57 -0400

    db query for all rows of candidates table

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,24 @@ const app = express();
 //middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MySQL username
+        user: 'root',
+        //your MySQL password
+        password: 'Ddiieetc61!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+//query database
+db.query(`SELECT * FROM candidates`, (err,rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
